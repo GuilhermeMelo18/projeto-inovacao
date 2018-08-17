aa5a5a1c6bab3714f8c5fa723745dffd3cd4cd29
João Nascimento <jn.marcos@outlook.com>
2018-08-16 22:01:25 -0300

    Apenas "ajeitamento" do app
    * Alteração do usuário. De "externo" passa a ser "pop".
    
    * Remoção da activity Main, drawer (estamos usando tabs basicamente) e menus que não serão usados.
    
    * Criação da PopulacaoActivity que passa a ser chamado pela tela Login.
    
    * Adição de strings.

---
+++ app/src/main/java/com/inovi/allerta/activity/PopulacaoActivity.java
@@ -0,0 +1,63 @@
+package com.inovi.allerta.activity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.inovi.allerta.R;
+import com.inovi.allerta.fragmentos.GraficoFragment;
+import com.inovi.allerta.fragmentos.PostFragment;
+import com.inovi.allerta.fragmentos.PublicacaoFragment;
+
+public class PopulacaoActivity extends AppCompatActivity {
+    private PopulacaoActivity.SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_populacao);
+        this.setTitle(R.string.titulo_populacao);
+
+        mSectionsPagerAdapter = new PopulacaoActivity.SectionsPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabsPopulacao);
+
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return new PublicacaoFragment();
+                case 1:
+                    return new GraficoFragment();
+                case 2:
+                    return new PostFragment();
+            }
+            return null;
+        }
+
+        //@Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+}
